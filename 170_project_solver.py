7f1e3fb3d98e97615a0b0f7f2553c1b041c1e174
Julia <julia.maimone@berkeley.edu>
2017-11-20 15:47:20 -0800

    added stuff from skeleton code

--- 170_project_solver.py
+++ 170_project_solver.py
@@ -1,3 +1,4 @@
+import argparse
 
 def solve(num_wizards, num_constraints, wizards, constraints): 
 	constraints_copy = constraints.copy()                   # copy of constraints since we'll be reducing
@@ -59,4 +60,40 @@ def solver(ordering, constraints_by_key): #recursive function that returns the o
 		if result != False
 			return result
 	return False
+
+
+def read_input(filename):
+    with open(filename) as f:
+        num_wizards = int(f.readline())
+        num_constraints = int(f.readline())
+        constraints = []
+        wizards = set()
+        for _ in range(num_constraints):
+            c = f.readline().split()
+            constraints.append(c)
+            for w in c:
+                wizards.add(w)
+                
+    wizards = list(wizards)
+    return num_wizards, num_constraints, wizards, constraints
+
+def write_output(filename, solution):
+    with open(filename, "w") as f:
+        for wizard in solution:
+            f.write("{0} ".format(wizard))
+
+""""
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description = "Constraint Solver.")
+    parser.add_argument("input_file", type=str, help = "___.in")
+    parser.add_argument("output_file", type=str, help = "___.out")
+    args = parser.parse_args()
+
+    num_wizards, num_constraints, wizards, constraints = read_input(args.input_file)
+    solution = solve(num_wizards, num_constraints, wizards, constraints)
+    write_output(args.output_file, solution)
+
+"""
+
 
